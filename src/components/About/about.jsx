cbde35f976e9289671e56759c4040c3944914480
sodiqscript111 <110563565+sodiqscript111@users.noreply.github.com>
2024-10-27 15:51:13 +0100

    Update about.jsx

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -58,13 +58,13 @@ const About = () => {
         <h1 ref={h1Ref}>ABOUT US</h1>
         <h2 ref={h2Ref}>Unveiling the Future of African Fashion</h2>
         <h3>
-          <b>At Walkerolarry Models</b>, we are dedicated to discovering and
+          <b>At Larry Walker Models</b>, we are dedicated to discovering and
           empowering the next generation of African models. Our mission is to
           provide aspiring talents with the platform, resources, and mentorship
           they need to excel in the global fashion industry.
         </h3>
         <p>
-          <b>Walkerolarry Models</b> is more than just a modeling agency; we are
+          <b>Larry Walker Models</b> is more than just a modeling agency; we are
           a gateway to success, where rising stars can thrive and make their
           mark on the international fashion stage.
         </p>
