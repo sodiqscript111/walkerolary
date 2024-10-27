a25aecffe0f5181689b74509e13386b8f6561edd
sodiqscript111 <110563565+sodiqscript111@users.noreply.github.com>
2024-10-27 15:55:48 +0100

    Update footer.jsx

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
             <footer className="footer">
                 <div className="footer-content">
                     <div className="footer-info">
-                        <p className="description">Walkerolarry Models is dedicated to discovering and empowering the next generation of African modeling talent.</p>
+                        <p className="description"> Larry walker Models is dedicated to discovering and empowering the next generation of African modeling talent.</p>
                         <div className="contact">
                             <h4>Contact</h4>
-                            <p>info@walkerolarrymodels.com</p>
+                            <p>lawmodelsacademy@gmail.com</p>
                             <p>+234 801 234 5678</p>
                         </div>
                         <div className="menu">
