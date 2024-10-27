f28d63b923e848685fa07d3e43e629b87e555647
sodiqscript111 <110563565+sodiqscript111@users.noreply.github.com>
2024-10-27 15:53:18 +0100

    Update academy.jsx

--- src/components/Academy/academy.jsx
+++ src/components/Academy/academy.jsx
@@ -6,7 +6,7 @@ const Academy = () =>{
         <div className="academy">
             <div className="textac">
                 <h1>Want to become a model ?</h1>
-                <p>At <b> Walkerolarry Models Academy </b>, we focus on nurturing and developing aspiring models with the skills and knowledge needed to succeed in the competitive fashion industry. Our academy offers a comprehensive curriculum that covers runway training, personal branding, portfolio building, and industry etiquette.
+                <p>At <b> Law Models Academy </b>, we focus on nurturing and developing aspiring models with the skills and knowledge needed to succeed in the competitive fashion industry. Our academy offers a comprehensive curriculum that covers runway training, personal branding, portfolio building, and industry etiquette.
 
 Our experienced coaches provide hands-on guidance, ensuring each model is well-prepared to excel on both local and global stages. Whether you're just starting out or looking to refine your skills, Walkerolarry Models Academy is the perfect place to take your modeling career to the next level.</p>
 <button className="button-39" role="button">
